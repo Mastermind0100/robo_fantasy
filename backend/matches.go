c1271e5f337aee3261e670de82ac064671467564
Anshuman Chhapolia <achhap.10.01@gmail.com>
2019-10-03 21:49:27 +0530

    Add CRUD for matches and test for CRUD
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

--- backend/matches.go
+++ backend/matches.go
@@ -4,16 +4,17 @@ import (
 	"context"
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
+	"sync"
 )
 
 // MatchStatus return different codes for different match status
 type MatchStatus int
 
 const (
-	Upcoming MatchStatus = 0 + iota
-	Red
-	Blue
-	Draw
+	StatusUpcoming MatchStatus = 0 + iota
+	StatusRed
+	StatusBlue
+	StatusDraw
 )
 
 // Match struct for matches
@@ -44,7 +45,7 @@ func GetLatestMatchCount() {
 	}
 	err := res.Decode(&mc)
 	if err != nil {
-		log.WithField("err", err).Error("Can't load read document from the database")
+		log.WithField("err", err).Error("Can't read matchcount document from the database")
 	}
 	s.mc = mc
 }
@@ -67,13 +68,13 @@ func UpdateMatchCount() {
 }
 
 func AddMatch(m Match) error {
-	s.mux.Lock()
-	defer s.mux.Unlock()
-
+	mux := sync.Mutex{}
 	matchCollection := s.db.Collection("match")
+	mux.Lock()
 	s.mc.Count++
 	m.MatchID = s.mc.Count
-
+	mux.Unlock()
+	UpdateMatchCount()
 	_, err := matchCollection.InsertOne(context.Background(), &m)
 	if err != nil {
 		log.WithField("err", err).Error("Can't insert Match document")
@@ -82,3 +83,80 @@ func AddMatch(m Match) error {
 	return nil
 }
 
+func ReadMatch(id int) *Match {
+	matchCollection := s.db.Collection("match")
+	filter := bson.D{{"matchid", id}}
+	res := matchCollection.FindOne(context.Background(), filter)
+	if res.Err() != nil {
+		log.WithFields(log.Fields{"err": res.Err(), "id": id}).Error("Can't read Match document")
+		return nil
+	}
+	var m Match
+	err := res.Decode(&m)
+	if err != nil {
+		log.WithField("err", err).Error("Can't decode match document")
+	}
+	return &m
+}
+
+func UpdateMatch(m Match) error {
+	matchCollection := s.db.Collection("match")
+	filter := bson.D{{"matchid", m.MatchID}}
+	update := bson.D{{
+		"$set", bson.D{
+			{"matchid", m.MatchID},
+			{"blueteam", m.BlueTeam},
+			{"readteam", m.RedTeam},
+			{"status", m.Status},
+			{"category", m.Category},
+		}}}
+
+	_, err := matchCollection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.WithFields(log.Fields{"err": err, "id": m.MatchID}).Error("Cant update match")
+		return err
+	}
+	return nil
+}
+
+func DeleteMatch(id int) error {
+	matchCollection := s.db.Collection("match")
+	filter := bson.D{{"matchid", id}}
+
+	_, err := matchCollection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.WithFields(log.Fields{"err": err, "id": id}).Error("Cant update match")
+		return err
+	}
+	return nil
+}
+
+func GetAllMatch() *[]Match {
+	matchCollection := s.db.Collection("match")
+	matches := make([]Match, 10)
+
+	res, err := matchCollection.Find(context.Background(), bson.D{})
+	if err != nil {
+		log.WithField("err", err).Error("Cant read the matches")
+		return nil
+	}
+
+	err = res.All(context.Background(), &matches)
+	if err != nil {
+		log.WithField("err", err).Error("Cant read decode all  matches")
+		return nil
+	}
+
+	return &matches
+}
+
+func UpdateStatus(id int, status MatchStatus) error {
+	m := ReadMatch(id)
+	m.Status = status
+	err := UpdateMatch(*m)
+	if err != nil {
+		log.WithFields(log.Fields{"err": err, "id": id}).Error("Can't update the match status")
+		return err
+	}
+	return nil
+}
---
+++ backend/matches_test.go
@@ -0,0 +1,68 @@
+package main
+
+import "testing"
+
+func TestAddMatch(t *testing.T) {
+	s.InitServer("robowarsTest")
+	err := AddMatch(Match{
+		MatchID:  0,
+		BlueTeam: "TestBlue",
+		RedTeam:  "TestRed",
+		Category: Sixty,
+		Status:   StatusUpcoming,
+	})
+	err = AddMatch(Match{
+		MatchID:  0,
+		BlueTeam: "TestBlue1",
+		RedTeam:  "TestRed1",
+		Category: Thirty,
+		Status:   StatusUpcoming,
+	})
+	if err != nil {
+		s.Shutdown()
+		t.Fail()
+	}
+	s.Shutdown()
+}
+
+func TestUpdateMatchCount(t *testing.T) {
+	s.InitServer("robowarsTest")
+	s.mc.Count = 0
+	UpdateMatchCount()
+}
+
+func TestReadMatch(t *testing.T) {
+	s.InitServer("robowarsTest")
+	m := ReadMatch(1)
+	t.Log(m)
+	s.Shutdown()
+}
+
+func TestGetAllMatch(t *testing.T) {
+	s.InitServer("robowarsTest")
+	m := GetAllMatch()
+	if m == nil {
+		t.Fail()
+	}
+	t.Log(m)
+	s.Shutdown()
+}
+
+func TestUpdateStatus(t *testing.T) {
+	s.InitServer("robowarsTest")
+	err := UpdateStatus(1, StatusBlue)
+	if err != nil {
+		t.Fail()
+	}
+	s.Shutdown()
+}
+
+func TestDeleteMatch(t *testing.T) {
+	s.InitServer("robowarsTest")
+	_ = DeleteMatch(1)
+	err := DeleteMatch(2)
+	if err != nil {
+		t.Fail()
+	}
+	s.Shutdown()
+}
