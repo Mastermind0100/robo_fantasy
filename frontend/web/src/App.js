c21bae56b48e239242564ef35b4a3610a96ff3ea
SilverSpear28 <abhijaymessi@gmail.com>
2019-10-08 19:12:24 +0530

    functionalities added

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import {Container} from "react-bootstrap";
+import Leaderboard from "../src/components/leaderboard.js";
+import Match from "./components/match.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="App">
-      
+        <Match />
+        <Leaderboard />
     </div>
   );
 }
