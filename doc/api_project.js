4ef77dd45b33d74f217d38a026baeeec047def22
Anshuman Chhapolia <achhap.10.01@gmail.com>
2019-10-10 15:24:45 +0530

    Update Docs
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

---
+++ doc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "robowars",
+  "version": "0.1.1",
+  "description": "Backend for robowars",
+  "title": "ROBOWARS",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-10-10T09:41:26.152Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
